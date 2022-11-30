0058c7a41ec7295024a014f743911829094e6303
Olga Gavrushenko <ogavby@gmail.com>
2022-11-30 13:42:07 -0800

    implementes show and delelete note by id

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const logger =require('./middleware/logger');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const logger = require("./middleware/logger");
 // const api = require('./routes/index.js');
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const dataFile = path.resolve(__dirname, "db", "db.json");
@@ -12,45 +12,70 @@ const dataFile = path.resolve(__dirname, "db", "db.json");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 // app.use('/api', api);
 
 // GET route for home page
-app.get('/', (req, res) => {
-  // res.sendFile(path.join(__dirname, 'public/index.html'));
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 //GET rote for notes page
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-app.get('/api/notes', (req, res) => {
+app.get("/api/notes", (req, res) => {
   const existingNotesString = fs.readFileSync(dataFile, "utf8");
   const existingNotes = JSON.parse(existingNotesString);
-  
   res.json(existingNotes);
   res.end();
-})
+});
 
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
   const { text, title } = req.body;
 
   const existingNotesString = fs.readFileSync(dataFile, "utf8");
   const existingNotes = JSON.parse(existingNotesString);
-  console.log(existingNotes);
 
   existingNotes.push({
     title,
     text,
     note_id: uuidv4(),
-  })
+  });
 
   fs.writeFileSync(dataFile, JSON.stringify(existingNotes, null, 4));
   res.end();
-})
+});
 
-app.listen(PORT, () => console.log(`Application started and listen on port ${PORT}`))
+// GET Route for a specific note
+app.get("/api/notes/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  const existingNotesString = fs.readFileSync(dataFile, "utf8");
+  const existingNotes = JSON.parse(existingNotesString);
+  const found = existingNotes.find((note) => note.note_id === noteId);
+  if (found) {
+    res.json(found);
+  } else {
+    res.status(400).send("No note with requested ID found");
+  }
+});
+
+// DELETE Route for a specific note
+app.delete("/api/notes/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  const existingNotesString = fs.readFileSync(dataFile, "utf8");
+  const existingNotes = JSON.parse(existingNotesString);
+
+  let result = existingNotes.filter((note) => note.note_id !== noteId);
+  result = JSON.stringify(result);
+
+  fs.writeFileSync(dataFile, result);
+  res.json(`Note has been deleted`);
+});
+
+app.listen(PORT, () =>
+  console.log(`Application started and listen on port ${PORT}`)
+);
